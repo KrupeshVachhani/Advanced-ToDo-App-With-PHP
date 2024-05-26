00aea8b738c94dcf1d4d8872be6d8508d0425a3e
Krupesh Vachhani <vachhani.krupesh@gmail.com>
2024-05-26 12:07:08 +0530

    Completed sliding sidebar with basic routing

---
+++ sidebar.php
@@ -0,0 +1,8 @@
+<div id="sidebar" class="sidebar">
+    <button id="toggle-sidebar" class="sidebar-toggle">☰</button>
+    <ul class="sidebar-menu">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="help.php">Help</a></li>
+        <li><a href="profile.php">Profile</a></li>
+    </ul>
+</div>
